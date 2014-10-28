a0938ac932e72f64d5f08aabb35e595aa27c29de
Mauro George <maurogot@gmail.com>
2014-10-28 19:39:01 -0200

    Use block on LhunValidator

--- lib/credit_card/luhn_validator.rb
+++ lib/credit_card/luhn_validator.rb
@@ -6,22 +6,35 @@ module CreditCard
 
     def initialize(credit_card)
       @credit_card = credit_card
+      @sum1 = @sum2 = 0
     end
 
     def call
-      s1 = s2 = 0
-      number.reverse.chars.each_slice(2) do |odd, even|
-        s1 += odd.to_i
-
-        double = even.to_i * 2
-        double -= 9 if double >= 10
-        s2 += double
+      pair_of_numbers do |odd, even|
+        do_first_sum(odd)
+        do_second_sum(even)
       end
-      (s1 + s2) % 10 == 0
+      (@sum1 + @sum2) % 10 == 0
     end
 
     private
 
       attr_reader :credit_card
+
+      def pair_of_numbers
+        number.reverse.chars.each_slice(2) do |odd, even|
+          yield(odd, even)
+        end
+      end
+
+      def do_first_sum(value)
+        @sum1 += value.to_i
+      end
+
+      def do_second_sum(value)
+        double = value.to_i * 2
+        double -= 9 if double >= 10
+        @sum2 += double
+      end
   end
 end
